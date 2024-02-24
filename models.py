51afe2238f3bf90f30c3f90e5d77b099a924d625
techopus <bhagatnitin254@gmail.com>
2024-02-24 20:28:58 +0100

    initial setup with data models

---
+++ models.py
@@ -0,0 +1,9 @@
+class Pitch:
+    def __init__(self, name, location, turf_type, last_maintenance_date, next_scheduled_maintenance, current_condition, replacement_date):
+        self.name = name
+        self.location = location
+        self.turf_type = turf_type
+        self.last_maintenance_date = last_maintenance_date
+        self.next_scheduled_maintenance = next_scheduled_maintenance
+        self.current_condition = current_condition
+        self.replacement_date = replacement_date
